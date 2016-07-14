9fe51d2c56341b387ea18aecc5be4b8ed3dd2503
Sebastian <sebastianecclesia@gmail.com>
2016-07-14 01:44:01 -0300

    Se agregan detalles al producto

--- src/main/java/org/ambar/app/administration/ProductoAdministrator.java
+++ src/main/java/org/ambar/app/administration/ProductoAdministrator.java
@@ -1,15 +1,53 @@
 package org.ambar.app.administration;
 
+import static org.lightadmin.api.config.utils.Editors.textArea;
+import static org.lightadmin.api.config.utils.Editors.wysiwyg;
+
 import org.ambar.app.domain.Producto;
 import org.lightadmin.api.config.AdministrationConfiguration;
 import org.lightadmin.api.config.builder.EntityMetadataConfigurationUnitBuilder;
+import org.lightadmin.api.config.builder.FieldSetConfigurationUnitBuilder;
+import org.lightadmin.api.config.builder.PersistentFieldSetConfigurationUnitBuilder;
 import org.lightadmin.api.config.unit.EntityMetadataConfigurationUnit;
+import org.lightadmin.api.config.unit.FieldSetConfigurationUnit;
+
 
 public class ProductoAdministrator extends AdministrationConfiguration<Producto> {
 	
 	@Override
     public EntityMetadataConfigurationUnit configuration(EntityMetadataConfigurationUnitBuilder configurationBuilder) {
         return configurationBuilder.nameField("codigo").singularName("Producto").pluralName("Productos").build();
+    }
+	
+	
+	public FieldSetConfigurationUnit listView( final FieldSetConfigurationUnitBuilder fragmentBuilder ) {
+        return fragmentBuilder
+        	.field( "imagen1" )
+    		.field("id")
+    		.field("codigo")
+    		.field("descripcion")
+    		.field("precio")
+    		.field("stock")
+            .build();
+    }
+	
+	
+	public FieldSetConfigurationUnit formView( final PersistentFieldSetConfigurationUnitBuilder fragmentBuilder ) {
+        return fragmentBuilder
+        		.field("id")
+        		.field("codigo")
+        		.field("descripcion")
+        		.field("detalles").editor( wysiwyg() )
+        		.field("pie").editor( textArea() )
+        		.field("precio")
+        		.field("stock")
+        		.field("categoria")
+        		.field( "imagen1" )
+        		.field( "imagen2" )
+        		.field( "imagenDetalle1" )
+        		.field( "imagenDetalle2" )
+        		.field( "imagenDetalle3" )
+                .build();
     }
 
 }
--- src/main/java/org/ambar/app/domain/Producto.java
+++ src/main/java/org/ambar/app/domain/Producto.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
 import org.lightadmin.api.config.annotation.FileReference;
@@ -23,6 +24,8 @@ public class Producto implements Serializable {
 
 	private String codigo;
 	private String descripcion;
+	private String detalles;
+	private String pie;
 	private BigDecimal precio;
 	private Integer stock;
 	private Categoria categoria;
@@ -34,7 +37,13 @@ public class Producto implements Serializable {
     private String imagen2;
 	
 	@FileReference(baseDirectory = "./target/classes/public")
-    private String imagen3;
+    private String imagenDetalle1;
+	
+	@FileReference(baseDirectory = "./target/classes/public")
+    private String imagenDetalle2;
+	
+	@FileReference(baseDirectory = "./target/classes/public")
+    private String imagenDetalle3;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -52,6 +61,23 @@ public class Producto implements Serializable {
 
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
+	}
+
+	@Lob
+	public String getDetalles() {
+		return detalles;
+	}
+
+	public void setDetalles(String detalles) {
+		this.detalles = detalles;
+	}
+
+	public String getPie() {
+		return pie;
+	}
+
+	public void setPie(String pie) {
+		this.pie = pie;
 	}
 
 	public BigDecimal getPrecio() {
@@ -103,11 +129,28 @@ public class Producto implements Serializable {
 		this.imagen2 = imagen2;
 	}
 
-	public String getImagen3() {
-		return imagen3;
+	public String getImagenDetalle1() {
+		return imagenDetalle1;
 	}
 
-	public void setImagen3(String imagen3) {
-		this.imagen3 = imagen3;
+	public void setImagenDetalle1(String imagenDetalle1) {
+		this.imagenDetalle1 = imagenDetalle1;
 	}
+
+	public String getImagenDetalle2() {
+		return imagenDetalle2;
+	}
+
+	public void setImagenDetalle2(String imagenDetalle2) {
+		this.imagenDetalle2 = imagenDetalle2;
+	}
+
+	public String getImagenDetalle3() {
+		return imagenDetalle3;
+	}
+
+	public void setImagenDetalle3(String imagenDetalle3) {
+		this.imagenDetalle3 = imagenDetalle3;
+	}
+
 }
