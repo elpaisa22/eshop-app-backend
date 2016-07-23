88d9ef1cacac9806e13099d3c1fa8ec94e9537ab
Sebastian <sebastianecclesia@gmail.com>
2016-07-23 01:01:36 -0300

    Se agrega pagina y tamaño de pagina como parametros

--- src/main/java/org/ambar/app/controller/ProductoController.java
+++ src/main/java/org/ambar/app/controller/ProductoController.java
@@ -1,12 +1,13 @@
 package org.ambar.app.controller;
 
-import java.util.List;
-
 import org.ambar.app.domain.Producto;
 import org.ambar.app.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +17,13 @@ public class ProductoController {
 	private ProductoRepository repository;
 	
 	@RequestMapping("/data/productos")
-	public List<Producto> getAll(){
-		return (List<Producto>) this.repository.findAll();
+	public Page<Producto> getAll(@RequestParam(value="page", defaultValue="1") Integer page,
+								 @RequestParam(value="limit", defaultValue="10") Integer limit,
+								 @RequestParam(value="sorting", defaultValue="") String sorting){
+		
+		PageRequest pageRequest = new PageRequest(page-1, limit);
+	
+		return this.repository.findAll(pageRequest);
 	}
 
 	@RequestMapping("/data/producto/{id}")
